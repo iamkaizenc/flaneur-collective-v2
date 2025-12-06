fb982034036575bdbd4952e1f9dda889f5ea327a
iamkaizenc <emirtufan33@gmail.com>
2025-12-06 21:16:36 +0300

    Optimize config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -3,11 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     formats: ["image/avif", "image/webp"],
-    unoptimized: false,
   },
   trailingSlash: false,
-  // Netlify compatibility
-  output: undefined, // Let Netlify plugin handle output
 };
 
 module.exports = nextConfig;
